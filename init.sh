5ec673e9ce5228b7c425a042db580977051646d6
kalsowerus <kalsowerus@protonmail.ch>
2022-03-29 19:33:50 +0200

    Use dir variable in paths

--- init.sh
+++ init.sh
@@ -29,24 +29,24 @@ link() {
 
 link .lesskey
 
-ZSHRC_INCLUDE="source $HOME/dotfiles/kalsowerus.zshrc"
+ZSHRC_INCLUDE="source $dir/kalsowerus.zshrc"
 ZSHRC_FILE="$HOME/.zshrc"
 if ! grep "^$ZSHRC_INCLUDE$" "$ZSHRC_FILE" >/dev/null 2>&1; then
     echo "$ZSHRC_INCLUDE" >> "$ZSHRC_FILE"
 fi
 
-GIT_INCLUDE_PATH="$HOME/dotfiles/kalsowerus.gitconfig"
+GIT_INCLUDE_PATH="$dir/kalsowerus.gitconfig"
 if command -v git >/dev/null 2>&1 && ! git config --global --list | grep "path=$GIT_INCLUDE_PATH$" >/dev/null; then
     git config --global --add include.path "$GIT_INCLUDE_PATH" 
 fi
 
-SSH_INCLUDE="Include $HOME/dotfiles/kalsowerus.ssh.conf"
+SSH_INCLUDE="Include $dir/kalsowerus.ssh.conf"
 SSH_CONFIG_FILE="$HOME/.ssh/config"
 if ! grep "^$SSH_INCLUDE$" "$SSH_CONFIG_FILE" >/dev/null 2>&1; then
     sed -i "1s:^:$SSH_INCLUDE\n:" "$SSH_CONFIG_FILE"
 fi
 
-VIMRC_INCLUDE="source $HOME/dotfiles/kalsowerus.vimrc"
+VIMRC_INCLUDE="source $dir/kalsowerus.vimrc"
 VIMRC_FILE="$HOME/.vimrc"
 if ! grep "^$VIMRC_INCLUDE$" "$VIMRC_FILE" >/dev/null 2>&1; then
     echo "$VIMRC_INCLUDE" >> "$VIMRC_FILE"
